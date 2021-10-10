2f81127db64a865f1bba390c6ad7d981652fb7e3
hhhhhhh2019 <47990344+hhhhhhh2019@users.noreply.github.com>
2021-10-10 10:52:01 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
 const cnv = document.getElementById("cnv");
-cnv.width = 500;
-cnv.height = 500;
+cnv.width = 360;
+cnv.height = 360;
 const ctx = cnv.getContext("2d");
+ctx.lineWidth = 0.5;
+
+var cell_type = 0;
+
+cnv.onmousedown = function(e) {
+	cells.push(new_cell(e.offsetX, e.offsetY, cell_type));
+}
 
 const CELL_RADIUS = 5;
 
 const colors = ["red", "green", "blue"];
 
-const rules = [
-//	 1,2,3
-	[1,0,0, 0,0,0], // 1
-	[0,0,0, 0,0,0], // 2
-	[0,0,0, 0,0,0], // 3
-]
+const rand = function(min, max) {
+	return Math.round(Math.random()*(max-min)) + min;
+}
 
-const cells = [];
+var rules = [
+	[1,1,0, 0,1,1, 1],
+	[1,1,1, 1,2,1, 3],
+	[1,1,1, 1,1,2, 2]
+];
+
+const generate_rules = function() {
+	rules = [
+		[rand(0,1),rand(0,1),rand(0,1), rand(0,4),rand(0,4),rand(0,4), rand(0,7)], // 1
+		[rand(0,1),rand(0,1),rand(0,1), rand(0,4),rand(0,4),rand(0,4), rand(0,7)], // 2
+		[rand(0,1),rand(0,1),rand(0,1), rand(0,4),rand(0,4),rand(0,4), rand(0,7)], // 3
+	];
+}
+
+//generate_rules();
+ 
+var cells = [];
 
 const new_cell = function(x, y, type) {
-	return {x, y, type, dx: 0, dy: 0, connections: []};
+	return {x, y, type, dx: 0, dy: 0, connected: [0,0,0], connections: []};
 }
 
-for (let i = 0; i < 500; i++) {
-	cells.push(new_cell(Math.random()*(cnv.width-CELL_RADIUS*2)+CELL_RADIUS, Math.random()*(cnv.height-CELL_RADIUS*2)+CELL_RADIUS, Math.floor(Math.random()*3)));
+const restart = function() {
+	cells = [];
+	
+	for (let i = 0; i < 200; i++) {
+		cells.push(new_cell(Math.random()*(cnv.width-CELL_RADIUS*2)+CELL_RADIUS, Math.random()*(cnv.height-CELL_RADIUS*2)+CELL_RADIUS, rand(0,2)));
+	}
 }
+
+//restart();
 
 const circle = function(x, y, color) {
 	ctx.fillStyle = color;
@@ -46,6 +72,13 @@ const distance = function(x1, y1, x2, y2) {
 const normalize = function(x, y) {
 	let l = distance(0, 0, x, y);
 	return [x / l, y / l];
+}
+
+const inArray = function(arr, val) {
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] == val) return true;
+	}
+	return false;
 }
 
 const collision = function() {
@@ -78,8 +111,8 @@ const collision = function() {
 
 			if (dist <= CELL_RADIUS * 2) {
 				can_move = false;
-				cell.x += (cell.x - cell2.x) * 0.05;
-				cell.y += (cell.y - cell2.y) * 0.05;
+				cell.x += (cell.x - cell2.x) * 0.1;
+				cell.y += (cell.y - cell2.y) * 0.1;
 			}
 		}
 
@@ -87,8 +120,8 @@ const collision = function() {
 			cell.x += cell.dx * 5;
 			cell.y += cell.dy * 5;
 		}
-		cell.dx *= 0.9;
-		cell.dy *= 0.9;
+		cell.dx *= 0.95;
+		cell.dy *= 0.95;
 	}
 }
 
@@ -102,26 +135,36 @@ const update = function() {
 
 	for (let i = 0; i < cells.length; i++) {
 		let cell = cells[i];
-		let neighbors = [0,0,0];
-		/*cell.dx += (Math.random() - 0.5) * 0.1;
-		cell.dy += (Math.random() - 0.5) * 0.1;*/
+		cell.connected = [0,0,0, 0];
+		cell.connections = [];
+		cell.dx += (Math.random() - 0.5) * 0.01;
+		cell.dy += (Math.random() - 0.5) * 0.01;
 		for (let j = 0; j < cells.length; j++) {
 			if (i == j) continue;
 			let cell2 = cells[j];
 
 			let dist = distance(cell.x, cell.y, cell2.x, cell2.y);
 			let connect = false;
+			let attract = false;
+			let can_connect = (rules[cell.type][cell2.type+3] > cell.connected[cell2.type] && rules[cell2.type][cell.type+3] > cell2.connected[cell.type]
+				&& rules[cell.type][6] > cell.connected[3] && rules[cell2.type][6] > cell2.connected[3])
+				//|| inArray(cell2.connections, cell);
 
-			if (dist < CELL_RADIUS * 4 && rules[cell.type][cell2.type]) {
-				neighbors[cell2.type]++;
-
-				if (rules[cell.type][cell2.type+3] > neighbors[cell2.type]) {
-					connect = true;
-					line(cell.x, cell.y, cell2.x, cell2.y);
-				}
+			if (dist < CELL_RADIUS * 3 && can_connect) {// && !inArray(cell.connections, cell2)) {
+				connect = true;
+				line(cell.x, cell.y, cell2.x, cell2.y);
+				cell.connected[cell2.type]++;
+				cell2.connected[cell.type]++;
+				cell.connected[3]++;
+				cell2.connected[3]++;
+				cell.connections.push(cell2);
+				cell2.connections.push(cell);
 			}
-
-			let [dx, dy] = [(cell2.x - cell.x) * (connect ? 1 : -1), (cell2.y - cell.y) * (connect ? 1 : -1)];
+			
+			//console.log(cell.connected);
+			attract = connect || (rules[cell.type][cell2.type] && can_connect) || inArray(cell2.connections, cell);
+			
+			let [dx, dy] = [(cell2.x - cell.x) * (attract ? 1 : -1), (cell2.y - cell.y) * (attract ? 1 : -1)];
 			[dx, dy] = normalize(dx, dy);
 			[dx, dy] = [dx / (dist ** 2) || 0, dy / (dist ** 2) || 0];
 
